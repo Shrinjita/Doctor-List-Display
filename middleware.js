895817738aa511d66f5a24dfcd48b8958af3a941
Shrinjita Paul <shrinjitapaul@gmail.com>
2024-02-28 10:36:57 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -1,14 +1,37 @@
-import { next } from '@vercel/edge';
 
-export default function middleware(req) {
-  return next({
-    headers: {
-      'Referrer-Policy': 'origin-when-cross-origin',
-      'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
-      'X-DNS-Prefetch-Control': 'on',
-      'Strict-Transport-Security':
-        'max-age=31536000; includeSubDomains; preload',
-    },
-  });
-}
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('/data')
+      .then(response => response.json())
+      .then(data => {
+          const userImagesContainer = document.getElementById('userImages');
+          const doctors = data.doctors;
+
+          doctors.forEach(doctor => {
+              const doctorBox = document.createElement('div');
+              doctorBox.classList.add('doctor-box');
+              doctorBox.onclick = function() {
+                  location.href = '#';
+              };
+
+              const img = document.createElement('img');
+              img.src = `img/${doctor.image}`;
+              img.alt = doctor.name;
+              img.classList.add('doctor-image');
+
+              const doctorName = document.createElement('div');
+              doctorName.classList.add('doctor-name');
+              doctorName.textContent = doctor.name;
+
+              const doctorSpecialty = document.createElement('div');
+              doctorSpecialty.classList.add('doctor-specialty');
+              doctorSpecialty.textContent = doctor.specialty;
+
+              doctorBox.appendChild(img);
+              doctorBox.appendChild(doctorName);
+              doctorBox.appendChild(doctorSpecialty);
+
+              userImagesContainer.appendChild(doctorBox);
+          });
+      })
+      .catch(error => console.error('Error loading JSON data:', error));
+});
